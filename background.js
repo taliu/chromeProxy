ac3c6a6d4901b721dc3ddeb61d2a78a82cb43c27
taliu <taliu@outlook.com>
2014-08-22 22:45:29 +0800

    web proxy repo init

---
+++ background.js
@@ -0,0 +1,38 @@
+﻿var config = {
+    mode: "direct",
+    rules: {
+        proxyForHttp: {
+            scheme: "http",
+            host: "127.0.0.1",
+            port: 8899
+        }
+    }
+};
+
+function getConfig(){
+	return {
+    mode: config.mode,
+	host:config.rules.proxyForHttp.host,
+	port: config.rules.proxyForHttp.port
+	};
+}
+
+function connectByProxy(host,port){
+  config.mode="fixed_servers";
+  config.rules.proxyForHttp.host=host;
+  config.rules.proxyForHttp.port=port;
+  setConfig(config);
+}
+
+function connectDirect(){
+ config.mode="direct";
+ setConfig(config);
+}
+
+function setConfig(config){
+	chrome.proxy.settings.set(
+		{value: config},
+		function(args) {
+		console.log("代理配置成功:",args);
+	});
+}
---
+++ popup.js
@@ -0,0 +1,42 @@
+﻿var page = chrome.extension.getBackgroundPage();
+$(function(){
+     var config=page.getConfig();
+	if(config.mode=="direct"){
+		$("#option").val(1);
+		$("#proxySetting").hide();
+	}else{
+	   $("#option").val(2);
+	   $("#proxySetting").show();
+	   $("#host").val(config.host);
+	   $("#port").val(config.port);
+	}
+})
+
+$("#option").click(function(){
+	if($(this).val()==2){
+		$("#proxySetting").show();
+	}else{
+		$("#proxySetting").hide();
+	}
+});
+
+$("#saveBtn").click(function(){
+	if($("#option").val()==2){
+		var host=$("#host").val();
+		var ipR=/((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/;//ip正则表达式
+		var domainR=/([a-zA-Z0-9]{1,20}\.){2}[a-zA-Z0-9]{1,20}/;//域名正则表达式
+		if(host=="localhost"||ipR.test(host)||domainR.test(host)){
+			var port=$("#port").val();
+			if(/\d{1,5}/.test(port)&&(+port)<65535){
+				page.connectByProxy(host,Number(port));
+			}else{
+				alert("端口有误");
+			}
+		}else{
+			alert("地址有误");
+		}
+	}else{
+		page.connectDirect();
+	}
+	alert("设置生效");
+});
